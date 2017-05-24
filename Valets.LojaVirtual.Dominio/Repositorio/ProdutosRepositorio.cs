08fb8e4a735a9a9880ec8fe0837e055bcd6fd607
Gabriel <gaperinetti@gmail.com>
2017-05-24 13:26:25 -0300

    Entity e Repositorio

---
+++ Valets.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valets.LojaVirtual.Dominio.Entidades;
+
+namespace Valets.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos {
+
+            get { return context.Produtos; }
+
+        }    
+
+    }
+}
---
+++ Valets.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Valets.LojaVirtual.Dominio.Repositorio;
+
+namespace Valets.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private ProdutosRepositorio repositorio;
+        
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            repositorio = new ProdutosRepositorio();
+            var produtos = repositorio.Produtos.Take(10);
+
+            return View(produtos);
+        }
+    }
+}
