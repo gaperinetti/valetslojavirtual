c5a27234936edcad572bdbfbfe477682a61271dd
Gabriel <gaperinetti@gmail.com>
2017-05-24 22:33:02 -0300

    Configuração RouteConfig

--- Valets.LojaVirtual.Web/App_Start/RouteConfig.cs
+++ Valets.LojaVirtual.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Valets.LojaVirtual.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Vitrine", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Vitrine", action = "ListaProdutos", id = UrlParameter.Optional }
             );
         }
     }
